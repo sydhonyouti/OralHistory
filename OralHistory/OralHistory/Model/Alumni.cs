5d52ce81d81e8091550c49c54719a82ef76ae475
Sydney H <64348633+sydhonyouti@users.noreply.github.com>
2022-12-10 18:21:30 -0600

    Alumni ViewModel with beginning data

---
+++ OralHistory/OralHistory/Model/Alumni.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OralHistory.Model
+{
+    public class Alumni
+    {
+        // This is all for the AlumniViewModel
+        public string IsAlumni { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MaidenName { get; set; }
+    }
+}
---
+++ OralHistory/OralHistory/ViewModels/AlumniViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using OralHistory.Model;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OralHistory.ViewModels
+{
+    public class AlumniViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // ViewModel has instance to model
+        private Alumni alumni;
+
+        public AlumniViewModel()
+        {
+            alumni = new Alumni();
+        }
+
+        // Public property to expose to the View
+
+    }
+}
